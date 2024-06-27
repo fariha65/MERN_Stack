8055073c40c11dd034bf5e385811494c1a20ee91
fariha65 <farihaafrintamanna@gmail.com>
2024-06-28 02:01:52 +0600

    commit 1

---
+++ MORDERNJS/creating_variable.js
@@ -0,0 +1,33 @@
+//var let const
+
+//var - window global obj ar any variable use var
+var n='fariha'
+n='afrin'
+alert(n)
+
+
+//let -window acess korte parena 
+let n1 ='fariha'
+n1 ='afrin'
+alert(n1)
+
+if(true) {
+    var n3 = "fariha"
+
+    alert(n3)
+}
+
+
+
+//window shoe n4 is not defined
+if(true) {
+    let n4 = "fariha"
+
+  
+}
+alert(n4)
+
+//const variable
+const n4 = "Fariha"
+n4="afrin"
+alert('n4')
---
+++ MORDERNJS/obj_DOM.js
@@ -0,0 +1,11 @@
+let heading = document.querySelector('h1')
+heading.innerText = "PHP"
+
+console.log(document.URL)
+
+window.alert('learning js')
+//storage create inspect> application>localhost
+window.localStorage.setItem('test','This is my test..')
+
+//local storage remove this data
+window.localStorage.removeItem('test')
